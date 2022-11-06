5e2fd687fcd66dd5a252a07f141c64f59e3fee39
Rasel291 <98130721+Rasel291@users.noreply.github.com>
2022-11-06 18:45:41 -0500

    Update testing.java

--- src/test/testing.java
+++ src/test/testing.java
@@ -1,35 +1,33 @@
 package test;
 
-import junit.framework.Assert;
-import org.junit.test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import model.Quotations;
+class testing {
+	@Test
+	  public void testGetTheLowestQuotation()
+	  {
+	    int sum =0;
+	    Quotations q = new Quotations();
+	     Object[] data = new Object[3];
+	       data[0] ="Fish-1";
+	       data[1]= "Beef-3";
+	       data[2]= "Chicken-14";
+	     Quotations[] lowest = q.getTheLowestQuotation(data);
+	    for(Quotations quo: lowest) {
+		            	if(quo != null){
+	        				
+	        				sum += Integer.parseInt(quo.getPrice()) * Integer.parseInt(quo.getQty());
+	        				
+	        				}
+	      int expected = 8900;
+	      
+	      Assert.assertEquals(expected, sum);
+	    }  
+	  }
+	
 
-public class testing {
-  
-
-  
-  @test
-  public void testGetTheLowestQuotation()
-  {
-    int sum =0;
-    Quotations q = new Quotations();
-     Object[] data = new Object[3];
-       data[0] ="Fish-5";
-       data[1]= "Beef-7";
-       data[2]= "Chicken-8";
-     Quotations[] lowest = q.getTheLowestQuotation(data);
-    for(Quotations quo: lowest) {
-	            	if(quo: !null){
-        				
-        				sum += Integer.parseInt(q.getPrice()) * Integer.parseInt(q.getQty());
-        				
-        				}
-      int expected = 4400;
-      
-      Assert.assertEquals(expected, sum);
-  
-  }
-  
-  
-  
 }
+
