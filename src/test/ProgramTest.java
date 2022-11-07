aca2b07f6a3139580b4258b68d07131e5a678179
Rasel291 <98130721+Rasel291@users.noreply.github.com>
2022-11-06 23:21:14 -0500

    Update ProgramTest.java

--- src/test/ProgramTest.java
+++ src/test/ProgramTest.java
@@ -35,6 +35,30 @@ class ProgramTest {
 		Assert.assertEquals(expected, sum);
 
 	}
+	
+	@Test
+	public void testCheckLogin() {
+		User u = new User();
+		String name = "2324";
+		String pwd = "123";
+
+		int[] arr = u.checkLogin(name, pwd);
+
+		int[] e = new int[4];
+		e[0] = 1; // user input
+		e[1] = 2; // vendor input
+		e[2] = 3; // admin input
+		e[3] = 4; // invalid input
+
+		if (arr[1] == 1)
+			Assert.assertEquals(e[0], arr[1]);
+		else if (arr[1] == 2)
+			Assert.assertEquals(e[1], arr[1]);
+		else if (arr[1] == 3)
+			Assert.assertEquals(e[2], arr[1]);
+		else
+			Assert.assertEquals(e[4], arr[1]);
+	}
 
 }
 
